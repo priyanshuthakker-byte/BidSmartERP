5f4b99d56e128043a8fe1e4a39e1c94fc9db9e51
Parthav B. Thakkar <priyanshutakkar.byte@gmail.com>
2025-10-22 15:05:15 +0530

    Fixed turbo config and added root layout

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
-/** @type {import(\'next\').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    turbo: false,
+    turbo: {
+      enabled: false,
+    },
   },
   reactStrictMode: true,
 }
